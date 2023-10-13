daf439be1e510a73b4e9c5333d3c1124f3df8f62
Mengyan Wu <mengyanw@umich.edu>
2023-10-13 14:31:24 -0400

    fix: #19 change cell edit event logic to avoid adding listeners repeatedly

--- src/cellEditEventProducer.ts
+++ src/cellEditEventProducer.ts
@@ -1,84 +1,93 @@
-import {
-    Notebook,
-  NotebookPanel,
-} from '@jupyterlab/notebook';
+import { Notebook, NotebookPanel } from '@jupyterlab/notebook';
 import { Cell, ICellModel } from '@jupyterlab/cells';
-import {
-    CodeMirrorEditor,
-} from '@jupyterlab/codemirror';
-import {
-  EditorView,
-  ViewUpdate
-} from "@codemirror/view";
+import { IObservableList } from '@jupyterlab/observables';
+import { CodeMirrorEditor } from '@jupyterlab/codemirror';
+import { EditorView, ViewUpdate } from '@codemirror/view';
 import { IJupyterLabPioneer } from './index';
 
 export class CellEditEventProducer {
-    static id: string = 'CellEditEvent'
+  static id: string = 'CellEditEvent';
 
-    listen(
-        notebookPanel: NotebookPanel,
-        pioneer: IJupyterLabPioneer,
-        logNotebookContentEvent: boolean
-    ) {
-        const cellEditEventHandler = 
-            async (_: Notebook, cell: Cell<ICellModel> | null) => {
-                await cell?.ready; // wait until cell is ready, to prevent errors when creating new cells
-                const editor = cell?.editor as CodeMirrorEditor
+  listen(
+    notebookPanel: NotebookPanel,
+    pioneer: IJupyterLabPioneer,
+    logNotebookContentEvent: boolean
+  ) {
+    const sendDoc = async (_: Notebook, cell: Cell<ICellModel> | null) => {
+      await cell?.ready; // wait until cell is ready, to prevent errors when creating new cells
+      const editor = cell?.editor as CodeMirrorEditor;
 
-                const event = {
-                    eventName: CellEditEventProducer.id,
-                    eventTime: Date.now(),
-                    eventInfo: {
-                        index: notebookPanel.content.widgets.findIndex(
-                            value => value === cell
-                        ),
-                        doc: editor?.state?.doc?.toJSON(), // send entire cell content if this is a new cell
-                    }
-                }; 
-                await pioneer.publishEvent(notebookPanel, event, logNotebookContentEvent);
+      const event = {
+        eventName: CellEditEventProducer.id,
+        eventTime: Date.now(),
+        eventInfo: {
+          index: notebookPanel.content.widgets.findIndex(
+            value => value === cell
+          ),
+          doc: editor?.state?.doc?.toJSON() // send entire cell content if this is a new cell
+        }
+      };
+      await pioneer.publishEvent(notebookPanel, event, logNotebookContentEvent);
+    };
 
-                editor?.injectExtension(EditorView.updateListener.of(
-                    async (v: ViewUpdate) => {
-                        if (v.docChanged) {
-                            const event = {
-                                eventName: CellEditEventProducer.id,
-                                eventTime: Date.now(),
-                                eventInfo: {
-                                    index: notebookPanel.content.widgets.findIndex(
-                                        value => value === cell
-                                    ),
-                                    changes: v.changes.toJSON() // only send changes afterwards
-                                }
-                            };
-                            await pioneer.publishEvent(notebookPanel, event, logNotebookContentEvent);
-                        }
-                    })
-                )
-            }
+    const addDocChangeListener = async (cell: Cell<ICellModel> | null) => {
+      await cell?.ready; // wait until cell is ready, to prevent errors when creating new cells
+      const editor = cell?.editor as CodeMirrorEditor;
 
-        cellEditEventHandler(notebookPanel?.content, notebookPanel?.content?.widgets[0]) 
-        // send entire cell content & add updatelistener for the first cell 
-        // since activeCellChanged is not triggered when notebook first opens
+      editor?.injectExtension(
+        EditorView.updateListener.of(async (v: ViewUpdate) => {
+          if (v.docChanged) {
+            const event = {
+              eventName: CellEditEventProducer.id,
+              eventTime: Date.now(),
+              eventInfo: {
+                index: notebookPanel.content.widgets.findIndex(
+                  value => value === cell
+                ),
+                changes: v.changes.toJSON() // send changes
+              }
+            };
+            await pioneer.publishEvent(
+              notebookPanel,
+              event,
+              logNotebookContentEvent
+            );
+          }
+        })
+      );
+    };
 
-        notebookPanel.content.activeCellChanged.connect(cellEditEventHandler);
-    }
+    notebookPanel?.content?.widgets.forEach(cell => {
+      addDocChangeListener(cell);
+    }); // add listener to existing cells
+    sendDoc(notebookPanel.content, notebookPanel.content.activeCell); // send initial active cell content
+    
+    notebookPanel.content.model?.cells.changed.connect(
+      async (_, args: IObservableList.IChangedArgs<ICellModel>) => {
+        if (args.type === 'add') {
+          addDocChangeListener(notebookPanel?.content?.widgets[args.newIndex]);
+        }
+      }
+    ); // add doc change listener to cells created after initialization
+    notebookPanel.content.activeCellChanged.connect(sendDoc); // send active cell content when active cell changes
+  }
 }
 
-    // type ThrottledFunction<T extends (...args: any) => any> = (...args: Parameters<T>) => ReturnType<T>;
-    // function throttle<T extends (...args: any) => any>(func: T, limit: number): ThrottledFunction<T> {
-    //     let inThrottle: boolean;
-    //     let lastResult: ReturnType<T>;
+// type ThrottledFunction<T extends (...args: any) => any> = (...args: Parameters<T>) => ReturnType<T>;
+// function throttle<T extends (...args: any) => any>(func: T, limit: number): ThrottledFunction<T> {
+//     let inThrottle: boolean;
+//     let lastResult: ReturnType<T>;
 
-    //     return function(this: any): ReturnType<T> {
-    //         const args = arguments;
-    //         const context = this;
+//     return function(this: any): ReturnType<T> {
+//         const args = arguments;
+//         const context = this;
 
-    //         if (!inThrottle) {
-    //             inThrottle = true;
-    //             setTimeout(() => (inThrottle = false), limit);
-    //             lastResult = func.apply(context, args);
-    //         }
+//         if (!inThrottle) {
+//             inThrottle = true;
+//             setTimeout(() => (inThrottle = false), limit);
+//             lastResult = func.apply(context, args);
+//         }
 
-    //         return lastResult;
-    //     };
-    // }
+//         return lastResult;
+//     };
+// }
